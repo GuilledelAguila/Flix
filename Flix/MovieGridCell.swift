cd7357ef9c3feee707ccaefc3be326f41a828435
Guillermo <guille@Guillermos-iMac.local>
2019-01-27 15:13:53 -0800

    Flix Part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Guillermo on 1/26/19.
+//  Copyright © 2019 Guillermo. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+}
